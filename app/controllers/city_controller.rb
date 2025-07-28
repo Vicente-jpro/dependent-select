0a5db2a8c21bf8234012b453d035a766c66ca917
vicente-jpro <vicenteviciii@outlook.com>
2025-07-28 15:28:07 +0100

    fix: form changed

--- app/controllers/city_controller.rb
+++ app/controllers/city_controller.rb
@@ -2,4 +2,18 @@ class CityController < ApplicationController
   def index
     @cities = City.where(province_id: params[:province_id])
   end
+
+  def create
+    @city = City.new(city_params)
+    if @city.save
+      redirect_to city_index_path(province_id: @city.province_id), notice: "City was successfully created."
+    else
+      render :cities
+    end
+  end
+
+  private
+  def city_params
+    params.require(:city).permit(:name_city, :province_id)
+  end
 end
